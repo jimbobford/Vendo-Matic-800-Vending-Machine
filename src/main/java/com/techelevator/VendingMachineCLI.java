e863dfd1e3dcc15383c33760d0840bc359263eab
Andy Chong Sam <andy@techelevator.com>
2023-07-26 20:21:54 -0400

    initial

---
+++ src/main/java/com/techelevator/VendingMachineCLI.java
@@ -0,0 +1,38 @@
+package com.techelevator;
+
+/*
+ * This class is provided to you as a *suggested* class to start
+ * your project. Feel free to refactor this code as you see fit.
+ */
+public class VendingMachineCLI {
+
+	private static final String MAIN_MENU_OPTION_DISPLAY_ITEMS = "Display Vending Machine Items";
+	private static final String MAIN_MENU_OPTION_PURCHASE = "Purchase";
+
+	public static void main(String[] args) {
+		VendingMachineCLI cli = new VendingMachineCLI();
+		cli.run();
+	}
+
+	public void run() {
+
+		while (true) {
+			/*
+			 * TODO: Use a method to initialize this value
+			 */
+			String choice = "";
+
+			if (choice.equals(MAIN_MENU_OPTION_DISPLAY_ITEMS)) {
+
+				// display vending machine items
+
+			} else if (choice.equals(MAIN_MENU_OPTION_PURCHASE)) {
+
+				// do purchase
+
+			}
+			// Remove the break when you've figured out how to setup the input loop
+			break;
+		}
+	}
+}
